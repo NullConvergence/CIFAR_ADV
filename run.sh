2782025d0868fc8c32095fff0210fa7ed5272caf
Alex <a.serban@cs.ru.nl>
2020-01-27 09:25:54 +0100

    Populated zoo and added more configs

---
+++ run.sh
@@ -0,0 +1,2 @@
+python -m pgd.train --config=configs/pgd_preact18.yml
+python -m pgd.train --config=configs/pgd_resnet18.yml
--- zoo/__init__.py
+++ zoo/__init__.py
@@ -1,1 +1,3 @@
+from .efficient_net import *
 from .resnet import *
+from .preact_resnet import *
