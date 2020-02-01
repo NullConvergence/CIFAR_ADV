bc72e35a74df608e587f9adffe34f75204702167
Alex <a.serban@cs.ru.nl>
2020-02-01 22:39:05 +0100

    Implemented resume

---
+++ resume/pgd.py
@@ -0,0 +1,75 @@
+import apex.amp as amp
+import argparse
+import torch
+import torch.nn as nn
+import torchvision
+from cifar_data import get_datasets
+from pgd.pgd_trainer import train, test
+import clean.trainer as clean
+from logger import Logger
+import utils
+import foolbox
+import numpy as np
+
+import foolbox.ext.native as fbn
+
+
+# TODO: import from cifar_data
+mean = [0.4914, 0.4822, 0.4465]
+std = [0.2471, 0.2435, 0.2616]
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', default='./eval/cnfg.yml', type=str)
+    return parser.parse_args()
+
+
+def main():
+    # config
+    args = parse_args()
+    cnfg = utils.parse_config(args.config)
+    # data
+    tr_loader, tst_loader = get_datasets(cnfg['data']['flag'],
+                                         cnfg['data']['dir'],
+                                         cnfg['data']['batch_size'],
+                                         apply_transform=False)
+    # initialization
+    utils.set_seed(cnfg['seed'])
+    device = torch.device(
+        'cuda:0') if cnfg['gpu'] is None else torch.device(cnfg['gpu'])
+    logger = Logger(cnfg)
+    model = utils.get_model(cnfg['model'])
+    checkpoint = torch.load(cnfg['resume']['path'])
+    model.load_state_dict(checkpoint['model'])
+    model.to(device)
+
+    criterion = nn.CrossEntropyLoss()
+    opt = torch.optim.SGD(model.parameters(),
+                          lr=cnfg['train']['lr'],
+                          momentum=cnfg['train']['momentum'],
+                          weight_decay=cnfg['train']['weight_decay'])
+    amp_args = dict(opt_level=cnfg['opt']['level'],
+                    loss_scale=cnfg['opt']['loss_scale'], verbosity=False)
+    if cnfg['opt']['level'] == '02':
+        amp_args['master_weights'] = cnfg['opt']['store']
+    model, opt = amp.initialize(model, opt, **amp_args)
+    scheduler = utils.get_scheduler(
+        opt, cnfg['train'], cnfg['train']['epochs']*len(tr_loader))
+    # train+test
+    for epoch in range(cnfg['train']['epochs']):
+        train(epoch, model, criterion,
+              opt, scheduler, cnfg, tr_loader, device, logger,
+              cnfg['train']['lr_scheduler'])
+        # testing
+        test(epoch, model, tst_loader, criterion, device, logger, cnfg, opt)
+        # save
+        if (epoch+1) % cnfg['save']['epochs'] == 0 and epoch > 0:
+            pth = 'models/' + cnfg['logger']['project'] + '_' \
+                + cnfg['logger']['run'] + '_' + str(epoch) + '.pth'
+            utils.save_model(model, cnfg, epoch, pth)
+            logger.log_model(pth)
+
+
+if __name__ == "__main__":
+    main()
